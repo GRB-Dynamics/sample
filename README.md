852fd2ef916e39505a0256e010b68f6d3734f30b
Blueobelisk <tariqramlall@gmail.com>
2015-04-30 19:11:45 -0400

    This is now a C# project.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Sample Repository
 
 Well Done Steven! - Devi
+
+Mind if I interrupt the conversation? Let's do a C# project.
+Have Visual Studio C# installed so we can play with some stuff.
+Check the main.cs file to chat more.
+-Tariq
---
+++ Sample_Project/main.cs
@@ -0,0 +1,14 @@
+﻿// Starting out with a simple stylish "hello world.
+/* Feel free to add on whatever and we'll see where this goes. */
+
+namespace BubbleGum // Initiate namespace.
+	{
+	public class Git // Initiate class.
+		{
+		static public void Main() // Program starts by reading the Main function.
+			{
+			System.Console.WriteLine("Hi Steven. Hi Murali. "); // Say hello world.
+			System.Console.ReadKey(); // Reads a character from the user. Put this in because Visual Studio will close console immediately after program runs.
+			}
+		}
+	}
