98738fd502a6e9a925fec081e22dffbb8eefa6ad
Murali Devi <profdevi@ymail.com>
2015-05-04 03:16:59 -0400

    Removed Line to press key
    1.  The last line in main is not required.  Read the comments in the
        code.  Next time when you running your app, use ctrl-f5.
    
    2.  Added git igore of all user defined files *.suo

--- Sample_Project/main.cs
+++ Sample_Project/main.cs
@@ -3,12 +3,17 @@
 
 namespace BubbleGum // Initiate namespace.
 	{
+	/////////////////////////////////////////////////////////////////////////////
 	public class Git // Initiate class.
 		{
 		static public void Main() // Program starts by reading the Main function.
 			{
-			System.Console.WriteLine("Hi Steven. Hi Murali. "); // Say hello world.
-			System.Console.ReadKey(); // Reads a character from the user. Put this in because Visual Studio will close console immediately after program runs.
+			System.Console.WriteLine("Hi Tariq. Hi Steven. Hi Murali. "); // Say hello world.
+
+			// You do not need this.  If you press F5(Debug Mode), execution does not stop.
+			// Press Ctrl-F5 when you are executing with out debugging code.
+			// System.Console.ReadKey();		// Reads a character from the user. Put this in 
+												// because Visual Studio will close console immediately after program runs.
 			}
 		}
 	}
