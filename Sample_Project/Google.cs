4750875e85d52df94db279f990bc4963832ff010
Blueobelisk <tariqramlall@gmail.com>
2015-05-07 23:46:41 -0400

    Added a function for user input.
    1. Added comments at the top of MainStartup.cs.
    
    2. Added a function to ask the user for our salary amounts.
    
    3. Added function to Google class to output total salary (if given the user entered salary amounts from (2)).
    
    4. Modified SayHello() function in Google.cs to output the individual's salary as well as their greeting.
    
    5. Added three calls to the function in (2) and one call to the function in (3) to the Main function.

--- Sample_Project/Google.cs
+++ Sample_Project/Google.cs
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////
 public class Google
 	{
-	private string mName;
+	private string mName;// I've seen this convention of adding m as a prefix for class variables in a C++ tutorial before.
 
 	//////////////////////////////////////////////
 	public Google(string name)
@@ -11,10 +11,18 @@ public class Google
 		}
 
 	//////////////////////////////////////////////
-	public void SayHello()
+	public void SayTotal(int tariqSalary, int steveSalary, int deviSalary)
 		{
-		string greetings=string.Format("Hello from {0}",mName);
-		System.Windows.Forms.MessageBox.Show(greetings);
+		int total = tariqSalary + steveSalary + deviSalary;
+		string money = string.Format("Combined we make ${0} per year.", total);
+		System.Windows.Forms.MessageBox.Show(money); 
+		}
+
+	//////////////////////////////////////////////
+	public void SayHello(int salary)
+		{
+		string greetings=string.Format("Hello from {0}. I make ${1} per year.", mName, salary);
+		System.Windows.Forms.MessageBox.Show(greetings); 
 		}
 	}
 
--- Sample_Project/MainStartup.cs
+++ Sample_Project/MainStartup.cs
@@ -1,17 +1,39 @@
-﻿
+﻿/// I was looking for API tutorials online but I didn't see anything really educational...
+/// If you know how to make an API in C# then let's do it Devi! 
 /////////////////////////////////////////////
 public class MainStartup
 	{
 	/////////////////////////////////////////
-	static public void Main()
+
+	static public int ReadSalary(string name)
 		{
+		System.Console.WriteLine("Please enter {0}'s salary. ", name);
+		string input = System.Console.ReadLine();
+		int number;
+		System.Int32.TryParse(input, out number);
+		return number;
+		}
+	
+	////////////////////////////////////////
+	static public void Main()
+		{ 
 		Google tariq=new Google("Tariq");
 		Google steve=new Google("Steve");
 		Google devi = new Google("Devi");
 
-		tariq.SayHello();
-		steve.SayHello();
-		devi.SayHello();
+		//Ask user for inputs. 
+		int tSalary = ReadSalary("Tariq");
+		int sSalary = ReadSalary("Steve");
+		int dSalary = ReadSalary("Devi");
+
+		tariq.SayHello(tSalary);
+		steve.SayHello(sSalary);
+		devi.SayHello(dSalary);
+
+		devi.SayTotal(tSalary, sSalary, dSalary); 
+		// Kind of sucks you have to specify the object to use a function that's in this class. 
+		// I tried to just use Google.SayTotal(tSalary, sSalary, dSalary); but that gives an error.
+
 		}
 	}
 
